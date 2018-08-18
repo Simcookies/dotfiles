c3e7dd35e13b7e1d2b24d1671e1b378f8ffa56ab
song <mssongan840136@outlook.com>
2018-08-18 22:14:38 +0900

    small fix

---
+++ lisp/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (lua-mode markdown-mode arduino-mode auto-complete smex material-theme web-mode rubocop projectile-rails)))
+ '(wakatime-cli-path "~/.emacs.d/wakatime/wakatime/cli.py"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
