9adc51b9d462c5aa84b77ba88a79979205da5223
song <mssongan840136@outlook.com>
2019-10-27 10:54:55 +0900

    change front-end intention

--- .emacs.d/lisp/init-frontend.el
+++ .emacs.d/lisp/init-frontend.el
@@ -10,6 +10,11 @@
           (lambda ()
             (setq web-mode-markup-indent-offset 2)
             (setq web-mode-css-indent-offset 2)
+            (setq web-mode-code-indent-offset 2)
+            (setq web-mode-script-padding 2)
+            (setq web-mode-enable-auto-closing t)
+            (setq web-mode-enable-auto-pairing t)
+            (setq web-mode-enable-current-column-highlight t)
             (rainbow-mode 1) ;; Show color with RGB value
             ))
 
