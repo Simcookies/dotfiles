2500283baa469f6100fe87132a2c6b3dfb2885ee
song <mssongan840136@outlook.com>
2020-03-12 09:58:45 +0900

    add setup config of pyvenv-activate

--- .emacs.d/lisp/init-python.el
+++ .emacs.d/lisp/init-python.el
@@ -8,6 +8,7 @@
              python-shell-interpreter "ipython"
              python-shell-interpreter-args "-i --simple-prompt")
             (setq elpy-rpc-virtualenv-path (quote current))
+            (pyvenv-activate (concat (getenv "PYENV_ROOT") "/versions/base"))
             (elpy-enable)))
 
 (provide 'init-python)
